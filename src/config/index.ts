6e11134bb953639dc95cc8a9852ab08263b630dd
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-25 00:02:08 +0600

    setup cloudinary

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,9 @@ interface IConfig {
   nodeEnv: string;
   database_url: string;
   redis_url: string;
+  cloudinary_cloud_name: string;
+  cloudinary_api_key: string;
+  cloudinary_api_secret: string;
 }
 
 const config: IConfig = {
@@ -13,6 +16,9 @@ const config: IConfig = {
   nodeEnv: process.env.NODE_ENV || "development",
   database_url: process.env.MONGODB_URL || "",
   redis_url: process.env.REDIS_URL || "",
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY || "",
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET || "",
 };
 
 export default config;
