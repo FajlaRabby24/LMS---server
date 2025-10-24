1253f8bc3e983378182ee74d98647b92496f6bdc
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-24 16:00:45 +0600

    config/index.ts updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,14 @@ dotenv.config();
 
 interface IConfig {
   port: number;
+  nodeEnv: string;
+  database_url: string;
 }
 
 const config: IConfig = {
   port: Number(process.env.PORT) || 5000,
+  nodeEnv: process.env.NODE_ENV || "development",
+  database_url: process.env.MONGODB_URL || "",
 };
 
 export default config;
