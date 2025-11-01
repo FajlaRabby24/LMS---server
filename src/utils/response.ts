7419a9bac0e2f4aa12365c1bb8b94ded13edb8b7
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-11-01 19:13:28 +0600

    sendSuccess response func created

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,16 +1,35 @@
 import { Response } from "express";
 import { ApiResponse } from "../@types/api";
 
+// --------- send error ----------
 export const sendError = (
   res: Response,
   message: string,
-  statusCode: number,
+  statusCode: number = 500,
   errors?: string[]
 ) => {
   const response: ApiResponse = {
     success: false,
     message,
     errors: errors || [],
+  };
+
+  return res.status(statusCode).json(response);
+};
+
+// --------- send success ----------
+export const sendSuccess = <T>(
+  res: Response,
+  message?: string,
+  data?: T,
+  statusCode: number = 200,
+  meta?: ApiResponse["meta"]
+) => {
+  const response: ApiResponse<T> = {
+    success: true,
+    message,
+    data,
+    meta,
   };
 
   return res.status(statusCode).json(response);
